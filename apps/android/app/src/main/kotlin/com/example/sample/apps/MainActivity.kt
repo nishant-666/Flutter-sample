b797283d1ab93dfcb2cd6725558c138b285a2e57
Nishant Kumar <nishants440@gmail.com>
2023-07-09 23:55:11 +0530

    Testing Flutter

---
+++ apps/android/app/src/main/kotlin/com/example/sample/apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sample.apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ apps/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Text('Hello! this is Nishant, and this is an Sample App!')
+  ));
+}
